a1e09e3f59f91cabf5ee4898b52b643de4590f14
amt-fidelis-mensah <fidelis.mensah@amalitech.org>
2024-02-05 12:05:45 +0000

    feat: Add structured folder for improved project organization

---
+++ src/app/Pages/Dashboard/Merchant/merchant-dashboard/merchant-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-merchant-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './merchant-dashboard.component.html',
+  styleUrl: './merchant-dashboard.component.scss'
+})
+export class MerchantDashboardComponent {
+
+}
