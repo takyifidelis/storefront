f5f9b073fa45b8916b6303b06fc8d03ea326c2ca
Felix7854 <felix.hagan@amalitech.org>
2024-02-11 15:39:36 +0000

    feat: added a home ecommerce feature

--- src/app/Pages/Authentication/Merchant/ecommerce-website/ecommerce-website.component.ts
+++ src/app/Pages/Authentication/Merchant/ecommerce-website/ecommerce-website.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faHeart } from '@fortawesome/free-regular-svg-icons';
 import { faSearch, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { HomeEcommerceComponent } from './home-ecommerce/home-ecommerce.component';
 
 @Component({
   selector: 'app-ecommerce-website',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, HomeEcommerceComponent],
   templateUrl: './ecommerce-website.component.html',
   styleUrl: './ecommerce-website.component.scss'
 })
---
+++ src/app/Pages/Authentication/Merchant/ecommerce-website/home-ecommerce/home-ecommerce.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-ecommerce',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-ecommerce.component.html',
+  styleUrl: './home-ecommerce.component.scss'
+})
+export class HomeEcommerceComponent {
+
+}
