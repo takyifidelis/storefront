9c901a98aaba1ec2dc3d315d2d6a160c139a101f
amt-fidelis-mensah <fidelis.mensah@amalitech.org>
2024-02-11 19:05:10 +0000

    feat: customizing the ecommerce navbar

--- src/app/Pages/Authentication/Merchant/ecommerce-website/ecommerce-website.component.ts
+++ src/app/Pages/Authentication/Merchant/ecommerce-website/ecommerce-website.component.ts
@@ -3,13 +3,14 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faHeart } from '@fortawesome/free-regular-svg-icons';
 import { faSearch, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import { HomeEcommerceComponent } from './home-ecommerce/home-ecommerce.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-ecommerce-website',
   standalone: true,
-  imports: [FontAwesomeModule, HomeEcommerceComponent],
+  imports: [FontAwesomeModule, HomeEcommerceComponent, MatIconModule],
   templateUrl: './ecommerce-website.component.html',
-  styleUrl: './ecommerce-website.component.scss'
+  styleUrl: './ecommerce-website.component.scss',
 })
 export class EcommerceWebsiteComponent {
   heartIcon = faHeart;
