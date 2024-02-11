465e76ceacc9b1401c07810d961219b8c5bd86b0
amt-fidelis-mensah <fidelis.mensah@amalitech.org>
2024-02-11 12:37:31 +0000

    bug: merchant signup configured correctly

--- src/app/Pages/Authentication/Merchant/signup/signup.component.ts
+++ src/app/Pages/Authentication/Merchant/signup/signup.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { ReactiveFormsModule, Validators } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faFacebook, faGoogle } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope, faEyeSlash } from '@fortawesome/free-regular-svg-icons';
 import { faCircle } from '@fortawesome/free-solid-svg-icons';
-import { FormGroup, FormControl, FormGroupDirective } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  FormGroupDirective,
+  ReactiveFormsModule,
+  Validators,
+  ValidationErrors,
+  AbstractControl,
+} from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../Auth/auth.service';
-import { provideHttpClient } from '@angular/common/http';
-import { withFetch } from '@angular/common/http';
 
 @Component({
   selector: 'app-signup-merchant',
@@ -30,18 +35,24 @@ export class SignupMerchantComponent {
   error: string | any = null;
 
   constructor(private authService: AuthService) {
-    this.signupForm = new FormGroup({
-      email: new FormControl('', [
-        Validators.required,
-        Validators.maxLength(128),
-        Validators.pattern(
-          /^[a-z0-9]+(?:\.[a-z0-9]+)*@[a-z0-9]+(?:\.[a-z0-9]+)+$/
-        ),
-      ]),
-      password: new FormControl('', Validators.required),
-      confirmPassword: new FormControl('', Validators.required),
-      businessName: new FormControl('', Validators.required),
-    });
+    this.signupForm = new FormGroup(
+      {
+        email: new FormControl('', [
+          Validators.required,
+          Validators.maxLength(128),
+          Validators.pattern(
+            /^[a-z0-9]+(?:\.[a-z0-9]+)*@[a-z0-9]+(?:\.[a-z0-9]+)+$/
+          ),
+        ]),
+        password: new FormControl('', [
+          Validators.required,
+          this.passwordValidator,
+        ]),
+        confirmPassword: new FormControl('', Validators.required),
+        businessName: new FormControl('', Validators.required),
+      },
+      { validators: this.confirmPasswordValidator }
+    );
   }
 
   onSubmit(form: FormGroupDirective) {
@@ -60,5 +71,55 @@ export class SignupMerchantComponent {
         this.error = errorMessage;
       }
     );
+    form.reset();
+  }
+  // Custom validator function for password strength and matching
+  passwordValidator(control: AbstractControl): ValidationErrors | null {
+    const value: string = control.value || '';
+    if (!value) {
+      return null; // Don't validate empty value
+    }
+    const hasUpperCase = /[A-Z]+/.test(value);
+    const hasLowerCase = /[a-z]+/.test(value);
+    const hasNumeric = /\d+/.test(value);
+    const hasMinLength = value.length >= 10;
+    const valid = hasUpperCase && hasLowerCase && hasNumeric && hasMinLength;
+    return !valid ? { passwordStrength: true } : null;
+  }
+  // Custom validator function for matching passwords
+  confirmPasswordValidator(control: AbstractControl): ValidationErrors | null {
+    const password = control.get('password')?.value;
+    const confirmPassword = control.get('confirmPassword')?.value;
+    if (password !== confirmPassword) {
+      return { passwordsNotMatching: true };
+    }
+    return null;
+  }
+
+  // Function to check various conditions of password
+  checkPasswordCondition(condition: RegExp): boolean {
+    const passwordControl = this.signupForm.get('password');
+    if (!passwordControl) {
+      return false;
+    }
+    const password = passwordControl.value || '';
+    return condition.test(password);
+  }
+
+  // Usage
+  containsLowerCase(): boolean {
+    return this.checkPasswordCondition(/[a-z]+/);
+  }
+
+  containsUpperCase(): boolean {
+    return this.checkPasswordCondition(/[A-Z]+/);
+  }
+
+  containsNumeric(): boolean {
+    return this.checkPasswordCondition(/\d+/);
+  }
+
+  containsMinTenChar(): boolean {
+    return this.checkPasswordCondition(/^.{10,}$/);
   }
 }
