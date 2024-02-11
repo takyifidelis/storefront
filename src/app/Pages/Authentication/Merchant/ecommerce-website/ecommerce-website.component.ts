0f937fdf54b7ab4fc8dd033622b9cef378db2b2d
amt-fidelis-mensah <fidelis.mensah@amalitech.org>
2024-02-11 21:29:34 +0000

    feat: customizing ecommerce nav bar

--- src/app/Pages/Authentication/Merchant/ecommerce-website/ecommerce-website.component.ts
+++ src/app/Pages/Authentication/Merchant/ecommerce-website/ecommerce-website.component.ts
@@ -4,11 +4,17 @@ import { faHeart } from '@fortawesome/free-regular-svg-icons';
 import { faSearch, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import { HomeEcommerceComponent } from './home-ecommerce/home-ecommerce.component';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-ecommerce-website',
   standalone: true,
-  imports: [FontAwesomeModule, HomeEcommerceComponent, MatIconModule],
+  imports: [
+    MatButtonModule,
+    FontAwesomeModule,
+    HomeEcommerceComponent,
+    MatIconModule,
+  ],
   templateUrl: './ecommerce-website.component.html',
   styleUrl: './ecommerce-website.component.scss',
 })
