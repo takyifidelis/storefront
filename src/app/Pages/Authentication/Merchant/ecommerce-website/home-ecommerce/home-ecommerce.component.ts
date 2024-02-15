52b5586d36ba8fe0e21a13291035db69da0692a0
amt-fidelis-mensah <fidelis.mensah@amalitech.org>
2024-02-15 19:26:35 +0000

    feat: template image link added

--- src/app/Pages/Authentication/Merchant/ecommerce-website/home-ecommerce/home-ecommerce.component.ts
+++ src/app/Pages/Authentication/Merchant/ecommerce-website/home-ecommerce/home-ecommerce.component.ts
@@ -12,23 +12,41 @@ import { CommonModule } from '@angular/common';
 })
 export class HomeEcommerceComponent {
   showInputLink: boolean = false;
-  url: any = 'assets/images/ecommerce-home-dresses.svg';
+
+  urls: { [key: number]: string } = {
+    1: 'assets/images/ecommerce-home-dresses.svg',
+    3: 'assets/images/ecommerce-home-tops.svg',
+    4: 'assets/images/ecommerce-home-accesories.svg',
+    5: 'assets/images/ecommerce-home-sneakers.svg',
+    6: 'assets/images/ecommerce-home-two-ladies.svg',
+  };
+
+  inputLinkVisibility: { [key: number]: boolean } = {
+    1: false,
+    2: false,
+    3: false,
+    4: false,
+    5: false,
+  };
 
   @ViewChild('fileInput') fileInput!: ElementRef;
 
-  openFileInput() {
-    this.fileInput.nativeElement.click();
+  openFileInput(fileInput: HTMLInputElement, index: number) {
+    fileInput.click();
+    // Set the visibility of the input link div associated with the clicked button to true
+    this.inputLinkVisibility[index] = true;
   }
 
   showLink() {
     this.showInputLink = !this.showInputLink;
   }
-  onselectFile(e: any) {
-    if (e.target.files && e.target.files.length > 0) {
+  onSelectFile(event: any, imageNumber: number) {
+    if (event.target.files && event.target.files.length > 0) {
       const reader = new FileReader();
-      reader.readAsDataURL(e.target.files[0]);
-      reader.onload = (event: any) => {
-        this.url = event.target.result;
+      reader.readAsDataURL(event.target.files[0]);
+      reader.onload = (e: any) => {
+        // Use the image number to update the corresponding URL in the `urls` object.
+        this.urls[imageNumber] = e.target.result;
       };
     }
   }
