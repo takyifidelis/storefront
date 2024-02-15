f16723862da0e62549a471027bea272391d8503f
amt-fidelis-mensah <fidelis.mensah@amalitech.org>
2024-02-15 14:23:05 +0000

    feat: template able to change an image

--- src/app/Pages/Authentication/Merchant/ecommerce-website/home-ecommerce/home-ecommerce.component.ts
+++ src/app/Pages/Authentication/Merchant/ecommerce-website/home-ecommerce/home-ecommerce.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home-ecommerce',
   standalone: true,
-  imports: [MatIconModule, MatButtonModule],
+  imports: [MatIconModule, MatButtonModule, CommonModule],
   templateUrl: './home-ecommerce.component.html',
   styleUrl: './home-ecommerce.component.scss',
 })
-export class HomeEcommerceComponent {}
+export class HomeEcommerceComponent {
+  showInputLink: boolean = false;
+  url: any = 'assets/images/ecommerce-home-dresses.svg';
+
+  @ViewChild('fileInput') fileInput!: ElementRef;
+
+  openFileInput() {
+    this.fileInput.nativeElement.click();
+  }
+
+  showLink() {
+    this.showInputLink = !this.showInputLink;
+  }
+  onselectFile(e: any) {
+    if (e.target.files && e.target.files.length > 0) {
+      const reader = new FileReader();
+      reader.readAsDataURL(e.target.files[0]);
+      reader.onload = (event: any) => {
+        this.url = event.target.result;
+      };
+    }
+  }
+}
