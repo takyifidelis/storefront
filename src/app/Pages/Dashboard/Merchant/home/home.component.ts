9d4ce40756d58b700a38043971c9b8589cd1c78d
amt-emmanuel-adomako <e.adomako@amalitech.org>
2024-02-17 21:47:46 +0000

    feat: merchant dashboard modified

--- src/app/Pages/Dashboard/Merchant/home/home.component.ts
+++ src/app/Pages/Dashboard/Merchant/home/home.component.ts
@@ -1,11 +1,5 @@
 import { MediaMatcher } from '@angular/cdk/layout';
-import {
-  Component,
-  ElementRef,
-  Inject,
-  OnInit,
-  ViewChild,
-} from '@angular/core';
+import {  Component, OnInit,} from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
@@ -13,20 +7,51 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatMenuModule } from '@angular/material/menu';
 import { Router, RouterModule } from '@angular/router';
-import { DOCUMENT } from '@angular/common';
 import { DataService } from '../../../../Services/data.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ ],
+  imports: [ 
+    MatProgressBarModule,MatButtonModule,
+    MatCardModule,MatIconModule,MatSidenavModule,
+    MatMenuModule,RouterModule, FormsModule,CommonModule 
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent implements OnInit {
+  oneOpened: boolean = false;
+  twoOpened: boolean = false;
+  threeOpened: boolean = false;
+  fourOpened: boolean = false;
   constructor(public dataService: DataService) {}
-
+  toggleOpenClose(key: number): void {
+    switch (key) {
+      case 1:
+        this.oneOpened = !this.oneOpened;
+        break;
+      case 2:
+        this.twoOpened = !this.twoOpened;
+        break;
+      case 3:
+        this.threeOpened = !this.threeOpened;
+        break;
+      case 4:
+        this.fourOpened = !this.fourOpened;
+        break;
+      
+        
+        
+      default:
+        break;
+    }
+  }
   ngOnInit() {
     this.dataService.merchantDashboardNoProjects = false
-    console.log('heeeii')
   }
 }
