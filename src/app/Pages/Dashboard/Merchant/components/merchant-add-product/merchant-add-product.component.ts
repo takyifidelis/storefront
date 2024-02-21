4ab8d0322e8d132465c3797b6d356e7e7710843e
amt-fidelis-mensah <fidelis.mensah@amalitech.org>
2024-02-21 16:27:21 +0000

    feat: merchant creating products and posting them

--- src/app/Pages/Dashboard/Merchant/components/merchant-add-product/merchant-add-product.component.ts
+++ src/app/Pages/Dashboard/Merchant/components/merchant-add-product/merchant-add-product.component.ts
@@ -1,12 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-merchant-add-product',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './merchant-add-product.component.html',
-  styleUrl: './merchant-add-product.component.scss'
+  styleUrl: './merchant-add-product.component.scss',
 })
 export class MerchantAddProductComponent {
+  images: string[] = []; // Assuming we don't need topImages and bottomImages arrays separately anymore
 
+  onFileSelected(event: any) {
+    const files: FileList = event.target.files;
+    if (files) {
+      const remainingSlots = 4 - this.images.length;
+      const filesToAdd = Math.min(files.length, remainingSlots);
+      for (let i = 0; i < filesToAdd; i++) {
+        const file = files.item(i);
+        if (file) {
+          const reader = new FileReader();
+          reader.onload = (e: any) => {
+            this.images.push(e.target.result);
+          };
+          reader.readAsDataURL(file);
+        }
+      }
+    }
+  }
+
+  showDragAndDrop(): boolean {
+    return this.images.length < 4;
+  }
 }
