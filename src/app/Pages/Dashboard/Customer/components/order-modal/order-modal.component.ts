8818e51a46d1703ad1207e110c2434dc22a0c31e
amt-fidelis-mensah <fidelis.mensah@amalitech.org>
2024-02-24 17:32:30 +0000

    func: Customer leave a review for a purchased product

--- src/app/Pages/Dashboard/Customer/components/order-modal/order-modal.component.ts
+++ src/app/Pages/Dashboard/Customer/components/order-modal/order-modal.component.ts
@@ -1,39 +1,57 @@
 import { Component } from '@angular/core';
 import { MatDialogModule } from '@angular/material/dialog';
 import { CommonModule } from '@angular/common';
+import { MatTabsModule } from '@angular/material/tabs';
+import { StarRatingComponent } from '../../../../Dashboard/Customer/components/star-rating/star-rating.component';
+import {
+  FormControl,
+  FormGroup,
+  FormGroupDirective,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { AuthService } from '../../../../Authentication/Auth/auth.service';
 
 @Component({
   selector: 'app-order-modal',
   standalone: true,
-  imports: [MatDialogModule, CommonModule],
+  imports: [
+    MatDialogModule,
+    CommonModule,
+    MatTabsModule,
+    StarRatingComponent,
+    ReactiveFormsModule,
+  ],
   templateUrl: './order-modal.component.html',
-  styleUrl: './order-modal.component.scss'
+  styleUrl: './order-modal.component.scss',
 })
 export class OrderModalComponent {
-active = true;
-active1 = false;
-active2 = false;
+  postReview: FormGroup;
+  error: string | any = null;
 
-isActive(val:number): void {
-  switch (val) {
-    case 1:
-    this.active = true
-    this.active1 = false
-    this.active2 = false
-    break;
-    case 2: 
-    this.active = false
-    this.active1 = true
-    this.active2 = false
-    break;
-    case 3:
-    this.active = false
-    this.active1 = false
-    this.active2 = true
-    break;
-    default:
-    break;
+  constructor(private authService: AuthService) {
+    this.postReview = new FormGroup({
+      remarks: new FormControl('', Validators.required),
+      comment: new FormControl('', Validators.required),
+    });
   }
-}
 
+  onSubmit(form: FormGroupDirective) {
+    console.log(form.value);
+    const product = '50c9690f-9623-4cf6-bc3f-f73069abdc93';
+    const rating = 3;
+    const remarks = form.value.remarks;
+    const comment = form.value.comment;
+    this.authService.reviewProduct(product, rating, remarks, comment).subscribe(
+      (resData) => {
+        console.log(resData);
+      },
+      (errorMessage) => {
+        console.log(errorMessage);
+        this.error = errorMessage;
+      }
+    );
+
+    form.reset();
+  }
 }
