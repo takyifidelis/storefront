48ab8940e7a4ce92c593e32234bab7f86fe1be83
amt-emmanuel-adomako <e.adomako@amalitech.org>
2024-03-01 11:04:22 +0000

    feat: created filter pipe to search for products on the store front

---
+++ src/app/Pipes/filter-product.pipe.ts
@@ -0,0 +1,35 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { DataService } from '../Services/data.service';
+
+@Pipe({
+  name: 'filterProduct',
+  standalone: true
+})
+export class FilterProductPipe implements PipeTransform {
+  constructor(private dataService:DataService){}
+  transform(values: any, searchTerm:string) {
+    const resultArray:any =[]
+    if(searchTerm === ''){
+      values = [...new Set(values)]
+      return values
+    }
+    if(searchTerm.length){
+      searchTerm = searchTerm.toLowerCase()
+          for (const item of values) {
+            if (item.name.toLowerCase().includes(searchTerm)) {
+              resultArray.push(item)
+            }
+          }
+        } 
+        return resultArray;
+      //   else {
+      //     this.dataService.doesNotExist.term = searchTerm
+      //     for (const item of values) {
+      //       if (item.name.toLowerCase().includes(searchTerm)) {
+      //         resultArray.push(item)
+      //       }
+      //     }
+      // } 
+  }
+
+}
