e6ce5ff62527e76072804b2e956c9b451efc7c77
amt-emmanuel-adomako <e.adomako@amalitech.org>
2024-03-01 10:22:23 +0000

    Added Angular router module, implemented ts interfaces and imported properties from dataservice

--- src/app/Pages/Authentication/Merchant/ecommerce-website/ecommerce-website.component.ts
+++ src/app/Pages/Authentication/Merchant/ecommerce-website/ecommerce-website.component.ts
@@ -1,10 +1,10 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faHeart, faStar } from '@fortawesome/free-regular-svg-icons';
 import { faSearch, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import { HomeEcommerceComponent } from './home-ecommerce/home-ecommerce.component';
 import { ReviewComponent } from './review/review.component';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
@@ -18,6 +18,9 @@ import {MatBadgeModule} from '@angular/material/badge';
 
 
 import {CdkDrag, CdkDragDrop, CdkDropList, DragDropModule, moveItemInArray} from '@angular/cdk/drag-drop';
+import { APIService } from '../../../../Services/api.service';
+import { FormsModule } from '@angular/forms';
+import { FilterProductPipe } from '../../../../Pipes/filter-product.pipe';
 
 @Component({
   selector: 'app-ecommerce-website',
@@ -37,25 +40,33 @@ import {CdkDrag, CdkDragDrop, CdkDropList, DragDropModule, moveItemInArray} from
     MatMenuModule,
     MatBadgeModule,
     DragDropModule,
+    FormsModule,
+    FilterProductPipe,
     CdkDropList, CdkDrag
   ],
   templateUrl: './ecommerce-website.component.html',
   styleUrl: './ecommerce-website.component.scss',
 })
-export class EcommerceWebsiteComponent {
+export class EcommerceWebsiteComponent implements OnInit{
   heartIcon = faHeart;
   shoppingCartIcon = faShoppingCart;
   searchIcon = faSearch;
-
+  serverResponseReceived = false
   dropDownMenu: HTMLElement | null = null;
 
   @ViewChild('fileInput') fileInput!: ElementRef;
-  constructor(public dataservice: DataService) {
-    if(dataservice.isInEditMode) {
-      dataservice.template = JSON.parse(localStorage.getItem('tempTemplate')!)
+  constructor(public dataService: DataService, private apiService:APIService, private router:Router) {
+    if(dataService.isInEditMode) {
+      
+      // console.log(dataService.template)
     }
   }
 
+  goToCheckout(){
+    // console.log(this.dataService.cart)
+    // console.log(JSON.parse(localStorage.getItem('cart')!))
+    this.router.navigate(['/checkout-page']);
+  }
   toggleBtnFunction() {
     this.dropDownMenu = document.querySelector('#dropdownMenu');
     if (this.dropDownMenu) {
@@ -68,8 +79,44 @@ export class EcommerceWebsiteComponent {
   }
 
   drop(event: CdkDragDrop<string[]>) {
-   if (this.dataservice.isEditable) {
-    moveItemInArray(this.dataservice.template.pagesOrder, event.previousIndex, event.currentIndex);
+   if (this.dataService.isEditable) {
+    moveItemInArray(this.dataService.template.pagesOrder, event.previousIndex, event.currentIndex);
    }
+  }
+  ngOnInit(): void {
+    // this.apiService.getMerchantStores(localStorage.getItem('businessId')!).subscribe((data:any) => {
+    //   console.log(data)
+    //   if (data.data) {
+    //     localStorage.setItem('storeId',data.data[0].id)
+    //     localStorage.setItem('tempTemplate',data.data[0].template.temp.options)
+    //     localStorage.setItem('template',data.data[0].template.options)
+    //     this.serverResponseReceived = true
+    //     if(this.dataService.isInEditMode){
+    //     this.dataService.template = JSON.parse(localStorage.getItem('tempTemplate')!)
+    //     }else{
+    //       this.dataService.template = JSON.parse(localStorage.getItem('template')!)
+    //     }
+    //     this.dataService.isLoading =false
+    //   } else {
+    //     this.router.navigate(['']);
+    //     this.dataService.isLoading =false
+    //   }
+    // })
+    this.apiService.getStore().subscribe((data:{[key: string]: any;} )=>{
+      console.log(data)
+      if (data['data']) {
+        localStorage.setItem('storeId',data['data'][2].id)
+        localStorage.setItem('template',data['data'][2].template.options)
+        this.serverResponseReceived = true
+          this.dataService.template = JSON.parse(localStorage.getItem('template')!)
+        this.dataService.isLoading =false
+      } else {
+        this.router.navigate(['']);
+        this.dataService.isLoading =false
+      }
+    // })
+    })
+   
+    console.log(this.dataService.template)
   }
 }
