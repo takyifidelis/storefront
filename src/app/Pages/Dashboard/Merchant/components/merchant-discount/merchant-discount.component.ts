ff5892c07f9a0e9cd29c7eb5304a8cb8c530354c
amt-fidelis-mensah <fidelis.mensah@amalitech.org>
2024-03-05 17:35:07 +0000

    feat: Added the discount form to the Merchant Dashboard

--- src/app/Pages/Dashboard/Merchant/components/merchant-discount/merchant-discount.component.ts
+++ src/app/Pages/Dashboard/Merchant/components/merchant-discount/merchant-discount.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { DataService } from '../../../../../Services/data.service';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faCheck, faFilter, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-merchant-discount',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, CommonModule],
   templateUrl: './merchant-discount.component.html',
   styleUrl: './merchant-discount.component.scss',
 })
@@ -13,5 +14,10 @@ export class MerchantDiscountComponent {
   filterIcon = faFilter;
   seaechICon = faSearch;
   checkIcon = faCheck;
+  showForm: boolean = false;
   constructor(public dataService: DataService) {}
+
+  toggleForm() {
+    this.showForm = !this.showForm;
+  }
 }
