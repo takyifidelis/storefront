a99e260ca3d562d387b08ca0197372a05b037eaf
amt-fidelis-mensah <fidelis.mensah@amalitech.org>
2024-03-06 13:33:34 +0000

    feat: added a model for the discount customization

--- src/app/Pages/Dashboard/Merchant/components/merchant-discount/merchant-discount.component.ts
+++ src/app/Pages/Dashboard/Merchant/components/merchant-discount/merchant-discount.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, Inject, ViewChild } from '@angular/core';
 import { DataService } from '../../../../../Services/data.service';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faCheck, faFilter, faSearch } from '@fortawesome/free-solid-svg-icons';
@@ -11,8 +11,17 @@ import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatInputModule } from '@angular/material/input';
 import { MatSort, MatSortModule } from '@angular/material/sort';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogContent,
+  MatDialogModule,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+import { MatTabsModule } from '@angular/material/tabs';
 import { SelectionModel } from '@angular/cdk/collections';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 export interface dummyUserInterface {
   checkbox: string;
@@ -35,6 +44,8 @@ export interface dummyUserInterface {
     MatSortModule,
     MatPaginatorModule,
     MatDialogModule,
+    HttpClientModule,
+    RouterModule,
   ],
   templateUrl: './merchant-discount.component.html',
   styleUrl: './merchant-discount.component.scss',
@@ -44,7 +55,9 @@ export class MerchantDiscountComponent {
   seaechICon = faSearch;
   checkIcon = faCheck;
   showForm: boolean = false;
-  constructor(public dataService: DataService, public dialog: MatDialog) {}
+  constructor(public dataService: DataService, public dialog: MatDialog) {
+    this.dataSource = new MatTableDataSource();
+  }
 
   displayedColumns: string[] = [
     'checkbox',
@@ -112,5 +125,30 @@ export class MerchantDiscountComponent {
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
+  }
+  moreVert(e: dummyUserInterface) {
+    this.dialog.open(MerchantDiscountCustomizeComponent, {
+      data: {
+        itemName: 'hat',
+        itemPrice: 'hat',
+      },
+      width: '479px',
+      position: { right: '50px', top: '10%' },
+    });
+  }
+}
+
+@Component({
+  selector: 'app-merchant-discount-customize',
+  standalone: true,
+  imports: [MatDialogTitle, MatDialogContent, MatTabsModule],
+  templateUrl:
+    'merchant-discount-customize/merchant-discount-customize.component.html',
+  styleUrl:
+    'merchant-discount-customize/merchant-discount-customize.component.scss',
+})
+export class MerchantDiscountCustomizeComponent {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: dummyUserInterface) {
+    console.log('kji');
   }
 }
