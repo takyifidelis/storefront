9a812f9f46c92a00a0ff0425b78193db9836b356
Felix7854 <felix.hagan@amalitech.org>
2024-03-08 16:54:22 +0000

    feat: added styling and a table to the wallet component and merchant-order component

--- src/app/Pages/Dashboard/Merchant/home/wallet/wallet.component.ts
+++ src/app/Pages/Dashboard/Merchant/home/wallet/wallet.component.ts
@@ -1,21 +1,65 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { environment } from '../../../../../../environments/environment.development';
 import { HttpClient } from '@angular/common/http';
 import { APIService } from '../../../../../Services/api.service';
+import { CommonModule, DatePipe } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { SelectionModel } from '@angular/cdk/collections';
+import { dummyUserInterface } from '../../components/merchant-products-dashboad/merchant-products-dashboad.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-wallet',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule,
+    FormsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatDialogModule,
+    MatTabsModule,
+    CommonModule],
   templateUrl: './wallet.component.html',
   styleUrl: './wallet.component.scss'
 })
+
 export class WalletComponent implements OnInit{
 walletForm!: FormGroup;
 walletInfo: any = [];
+displayedColumns: string[] = [
+  'paymentMethod',
+  'name',
+  'walletId',
+  'date',
+];
+dataSource: MatTableDataSource<dummyUserInterface>;
+selection = new SelectionModel<dummyUserInterface>(true, []);
+@ViewChild(MatPaginator) paginator!: MatPaginator;
+@ViewChild(MatSort) sort!: MatSort;
+users: dummyUserInterface[] = [];
+datepipe: DatePipe = new DatePipe('en-US');
+isHomeActive: boolean = false;
+isPaymentActive: boolean = false;
 
-constructor(public http: HttpClient, private apiService: APIService){}
+constructor(public http: HttpClient, private apiService: APIService, public dialog: MatDialog, private router: Router){
+  this.dataSource = new MatTableDataSource(this.users);
+
+}
 
   ngOnInit(): void {
     this.walletForm = new FormGroup({
@@ -27,7 +71,19 @@ constructor(public http: HttpClient, private apiService: APIService){}
     this.apiService.getWallet('f9586428-62e3-4455-bb1d-61262a407d1a').subscribe((res: any)=> {
       console.log(res)
       this.walletInfo = res.data;
+    this.dataSource = new MatTableDataSource(this.walletInfo);
     })
+
+    this.isHomeActive = true;
+  }
+
+  onClick(): void {
+    if(this.isHomeActive){
+      this.isHomeActive = true;
+      this.isPaymentActive = false;
+this.router.navigate(['/merchant/home']);
+    }
+    
   }
 
   onSubmit(){
@@ -43,5 +99,40 @@ constructor(public http: HttpClient, private apiService: APIService){}
       .subscribe((res: any) => {
         console.log(res);
       });
+  }
+
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+  showSelection(e: any) {
+    e.stopPropagation();
+    console.log(this.selection.selected);
+  }
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  toggleAllRows() {
+    if (this.isAllSelected()) {
+      this.selection.clear();
+      console.log(this.selection.selected);
+      return;
+    }
+
+    this.selection.select(...this.dataSource.data);
+    console.log(this.selection.selected);
+  }
+
+  /** The label for the checkbox on the passed row */
+  checkboxLabel(row?: dummyUserInterface): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'deselect' : 'select'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${
+      row.checkbox + 1
+    }`;
+  }
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
 }
