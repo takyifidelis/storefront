93b34c6bf24ae065f65d8df3b3f3ce83eefcbb23
amt-fidelis-mensah <fidelis.mensah@amalitech.org>
2024-03-08 16:27:57 +0000

    feat: Created customer table and a modal

--- src/app/Pages/Dashboard/Merchant/components/merchant-customers/merchant-customers.component.ts
+++ src/app/Pages/Dashboard/Merchant/components/merchant-customers/merchant-customers.component.ts
@@ -1,11 +1,16 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, Inject, OnInit, ViewChild } from '@angular/core';
 import { DataService } from '../../../../../Services/data.service';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatTabsModule } from '@angular/material/tabs';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogModule,
+} from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
@@ -18,9 +23,16 @@ import { SelectionModel } from '@angular/cdk/collections';
 import { faFilter, faSearch, faCheck } from '@fortawesome/free-solid-svg-icons';
 import { APIService } from '../../../../../Services/api.service';
 import {
-  dummyUserInterface,
   MerchantProductDiscountComponent,
+  dummyUserInterface,
 } from '../merchant-products-dashboad/merchant-products-dashboad.component';
+import { dummyUserInterface1 } from '../../../../../interface/dummy-user.model';
+
+export interface productInterface {
+  name: string;
+  price: number;
+  id: string;
+}
 
 @Component({
   selector: 'app-merchant-customers',
@@ -50,16 +62,16 @@ export class MerchantCustomersComponent implements OnInit {
   checkIcon = faCheck;
   displayedColumns: string[] = [
     'checkbox',
-    'name and email',
-    'orderId',
-    'date',
-    'status',
-    'price',
+    'name',
+    'email',
+    'orders',
+    'lastOrder',
     'bubble',
   ];
+  users: any;
   customers: any;
-  dataSource: MatTableDataSource<dummyUserInterface>;
-  selection = new SelectionModel<dummyUserInterface>(true, []);
+  dataSource: MatTableDataSource<dummyUserInterface1>;
+  selection = new SelectionModel<dummyUserInterface1>(true, []);
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
@@ -68,15 +80,15 @@ export class MerchantCustomersComponent implements OnInit {
     private apiService: APIService,
     public dataService: DataService
   ) {
-    this.dataSource = new MatTableDataSource();
+    this.dataSource = new MatTableDataSource(this.users);
   }
   moreVert(e: dummyUserInterface) {
-    this.dialog.open(MerchantProductDiscountComponent, {
+    this.dialog.open(CustomerDetailsComponent, {
       data: {
         itemName: 'hat',
         itemPrice: 'hat',
       },
-      width: '479px',
+      width: '600px',
       position: { right: '50px', top: '10%' },
     });
     // console.log(e);
@@ -105,7 +117,7 @@ export class MerchantCustomersComponent implements OnInit {
   }
 
   /** The label for the checkbox on the passed row */
-  checkboxLabel(row?: dummyUserInterface): string {
+  checkboxLabel(row?: dummyUserInterface1): string {
     if (!row) {
       return `${this.isAllSelected() ? 'deselect' : 'select'} all`;
     }
@@ -119,10 +131,40 @@ export class MerchantCustomersComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.apiService.getCustomers().subscribe((res: any)=> {
+    this.apiService.getCustomers().subscribe((res: any) => {
       this.customers = res.data;
-console.log(this.customers)
-    this.dataSource = new MatTableDataSource(this.customers);
-    })
+      console.log(this.customers);
+      this.dataSource = new MatTableDataSource([
+        {
+          checkbox: '',
+          name: 'ddjkjff',
+          email: 'hello@',
+          orders: 'none',
+          lastOrder: 'dd',
+          bubble: '',
+        },
+        {
+          checkbox: '',
+          name: 'ddjkjff',
+          email: 'hello@',
+          orders: 'none',
+          lastOrder: 'dd',
+          bubble: '',
+        },
+      ]);
+    });
   }
+}
+
+@Component({
+  selector: 'app-customer-details',
+  standalone: true,
+  imports: [MatTabsModule],
+  templateUrl: 'customer-details/customer-details.component.html',
+  styleUrl: 'customer-details/customer-details.component.scss',
+})
+export class CustomerDetailsComponent {
+  productDataSource!: MatTableDataSource<productInterface>;
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: productInterface) {}
 }
