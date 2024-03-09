d68303249c5a9088abf4a45f1f9b46a3d3f71fae
amt-emmanuel-adomako <e.adomako@amalitech.org>
2024-03-09 15:31:19 +0000

    feat:installing intro.js library and setting it up

--- src/app/Pages/Dashboard/Merchant/merchant-dashboard/merchant-dashboard.component.ts
+++ src/app/Pages/Dashboard/Merchant/merchant-dashboard/merchant-dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
@@ -7,7 +7,7 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatMenuModule } from '@angular/material/menu';
 import { DataService } from '../../../../Services/data.service';
-
+import introJs from 'intro.js';
 import { MerchantAddProductComponent } from '../components/merchant-add-product/merchant-add-product.component';
 import { AuthService } from '../../../Authentication/Auth/auth.service';
 import { APIService } from '../../../../Services/api.service';
@@ -29,7 +29,8 @@ import { APIService } from '../../../../Services/api.service';
   templateUrl: './merchant-dashboard.component.html',
   styleUrl: './merchant-dashboard.component.scss',
 })
-export class MerchantDashboardComponent {
+export class MerchantDashboardComponent implements OnInit {
+  introJS: any;
   constructor(
     public dataService: DataService,
     private authService: APIService,
@@ -46,5 +47,34 @@ export class MerchantDashboardComponent {
         console.error('Logout error:', error);
       }
     );
+  }
+  ngOnInit() {
+    this.introJS = introJs();
+    this.introJS.setOptions({
+      steps: [
+        {
+          element: '#tourStepOne',
+          intro: 'This is the first step',
+          position:'right',
+          introHTML: `
+            <div>
+              <h3>Step 1: Introduction</h3>
+              <p>This is the first step of the tour.</p>
+              <ul>
+                <li>Point 1</li>
+                <li>Point 2</li>
+                <li>Point 3</li>
+              </ul>
+            </div>
+          `
+        },
+        {
+          element: '#step2',
+          intro: 'This is the second step'
+        }
+      ]
+    });
+    this.introJS.start();
+
   }
 }
