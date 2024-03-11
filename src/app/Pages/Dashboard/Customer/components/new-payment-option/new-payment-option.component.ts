a2127da16c079173553920e9031353aa20c8450c
amt-fidelis-mensah <fidelis.mensah@amalitech.org>
2024-03-11 12:34:16 +0000

    feat: customize the payment option dashboard of customer to display available payment methods

--- src/app/Pages/Dashboard/Customer/components/new-payment-option/new-payment-option.component.ts
+++ src/app/Pages/Dashboard/Customer/components/new-payment-option/new-payment-option.component.ts
@@ -1,5 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormGroupDirective,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatTabsModule } from '@angular/material/tabs';
@@ -10,6 +17,7 @@ import {
   faCcVisa,
 } from '@fortawesome/free-brands-svg-icons';
 import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { APIService } from '../../../../../Services/api.service';
 
 @Component({
   selector: 'app-new-payment-option',
@@ -20,6 +28,7 @@ import { faCheck } from '@fortawesome/free-solid-svg-icons';
     CommonModule,
     MatTabsModule,
     MatRadioModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './new-payment-option.component.html',
   styleUrl: './new-payment-option.component.scss',
@@ -30,4 +39,40 @@ export class NewPaymentOptionComponent {
   check = faCheck;
   paypal = faCcPaypal;
   active: boolean | undefined;
+  addPaypal: FormGroup;
+
+  constructor(private apiService: APIService) {
+    this.addPaypal = new FormGroup({
+      method: new FormControl('Paypal'),
+      walletId: new FormControl('', Validators.required),
+      name: new FormControl('', Validators.required),
+      secret: new FormControl('', Validators.required),
+    });
+  }
+  AddPaypalMethod(form: FormGroupDirective) {
+    console.log(form.value);
+    if (!form.valid) {
+      return;
+    }
+    const method = form.value.method;
+    const walletId = form.value.walletId;
+    const name = form.value.name;
+    const secret = form.value.secret;
+    this.apiService
+      .addWalletCustomer(
+        method,
+        walletId,
+        name,
+        secret,
+        localStorage.getItem('customerId')!
+      )
+      .subscribe(
+        (resData) => {
+          console.log(resData);
+        },
+        (errorMessage) => {
+          console.log(errorMessage);
+        }
+      );
+  }
 }
