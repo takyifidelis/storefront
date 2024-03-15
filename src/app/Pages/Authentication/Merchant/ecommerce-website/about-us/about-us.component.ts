e3023f498a474a92ae75a2295b2a7a5ae443c518
amt-emmanuel-adomako <e.adomako@amalitech.org>
2024-03-15 13:28:31 +0000

    feat: made text content editable

--- src/app/Pages/Authentication/Merchant/ecommerce-website/about-us/about-us.component.ts
+++ src/app/Pages/Authentication/Merchant/ecommerce-website/about-us/about-us.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { DataService } from '../../../../../Services/data.service';
+import { MatDialog } from '@angular/material/dialog';
+import { TemplateTextEditorDialogComponent } from '../../../../Dashboard/Merchant/components/template-text-editor-dialog/template-text-editor-dialog.component';
 
 @Component({
   selector: 'app-about-us',
@@ -7,4 +10,19 @@ import { Component } from '@angular/core';
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.scss',
 })
-export class AboutUsComponent {}
+export class AboutUsComponent {
+  constructor(public dataService: DataService, public dialog: MatDialog){}
+  editText(...args: string[]) {
+    // console.log(text,args )
+    if(this.dataService.isEditingTemp){
+      this.dialog.open(TemplateTextEditorDialogComponent, {
+        data: args,
+        hasBackdrop: false
+      }).afterClosed().subscribe((editedtText) => {
+        args.shift()
+        this.dataService.updateText(editedtText, ...args);
+        console.log({editedTex:editedtText}, ...args);
+      });
+    }
+}
+}
