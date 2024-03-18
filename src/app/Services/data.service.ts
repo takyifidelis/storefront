e17f234045ecadc163e3cd4e7ee979b80ad1dea8
amt-emmanuel-adomako <e.adomako@amalitech.org>
2024-03-18 05:07:47 +0000

    feat: added functionality to modify text in the template editor

--- src/app/Services/data.service.ts
+++ src/app/Services/data.service.ts
@@ -1,27 +1,15 @@
 import { Injectable } from '@angular/core';
 import { UserCredentials, merchantProduct } from '../interfaces/all-interfaces';
+import { MatDialog } from '@angular/material/dialog';
+import { ImageLinkComponent } from '../Pages/Dashboard/Merchant/components/image-link/image-link.component';
 
 @Injectable({
   providedIn: 'root',
 })
 
-// merchantDashboardNoProjects: boolean = false;
-// loginCredentials:UserCredentials = {email:'', password:''}
-// doesNotExist = {exist: false, term:''}
-// isMobileBool: boolean = false;
-// isEditable: boolean = false;
-// customerId: string = localStorage.getItem("customerId") || '';
-// orderId: string =''
-// storeId: string = ''
-// //   stores = JSON.parse(localStorage.getItem("stores")!)|| '' ;
-// productId: string = '';
-// merchantBusinessType1: string =localStorage.getItem("storeType") || '';
-// businessId: string = localStorage.getItem("businessId") || ''
-// merchantStoreName: string = localStorage.getItem("storeName") || ''
-// showInputLink: boolean = false;
-// isLoading: boolean = false
-export class DataService {
 
+export class DataService {
+  storeProducts:any = []
 like: any =  [];
   cart: any = [];
   updateProduct:unknown 
@@ -36,6 +24,7 @@ like: any =  [];
   isMobileBool: boolean = false;
   isLoading = false;
   isEditable: boolean = false;
+  isEditingTemp: boolean = false;
 
   customerId: string = '';
   orderId: string = '';
@@ -165,9 +154,9 @@ like: any =  [];
       '16px',
       '18px',
     ],
-    primaryColor: 'violet',
-    primaryBackground: '#f5f5f5',
-    navbarBackgroundColor: '#212529',
+    primaryColor: '1C222B',
+    primaryBackground: '#FFFFFF',
+    navbarBackgroundColor: '#000000',
     navbarColor: '#fff',
     body: {
       fontSize: '16px',
@@ -178,19 +167,18 @@ like: any =  [];
       fontWeight: '400',
     },
     buttons: {
-      borderColor: 'green',
+      borderColor: '#1C222B',
       borderRadius: '0px',
-      color: 'blue',
+      color: '#1C222B',
       backgroundColor: 'pink',
       padding: '1rem 3rem',
     },
     templateImages: {
       heroImage: '/assets/images/shopping-image.jpeg',
       promoImage: 'assets/images/ecommerce-home-two-ladies.svg',
-      aboutHeroImage: 'https://gitinspired.s3.amazonaws.com/aboutUsHero.jpg',
-      aboutFooterImage:
-        'https://gitinspired.s3.amazonaws.com/aboutFooterImage.jpg',
-      contactImage: 'https://gitinspired.s3.amazonaws.com/contactUsImage.png',
+      aboutHeroImage: '/assets/images/ecommerce-about-us-header-image.svg',
+      aboutFooterImage:'/assets/images/about-us-last-image.svg',
+      contactImage: '/assets/images/about-us-backgroud.svg',
     },
     textAlign: 'center',
     headings: {
@@ -210,83 +198,7 @@ like: any =  [];
           justifyContent: 'center',
           flexDirection: 'column',
         },
-
-        //   template ={
-        //     id: "template-1",
-        //     screenshot: "/assets/images/Text.svg",
-        //     text: "ShopNest",
-        //     fontFamily: "Inter",
-        //     fontArray: ["Inter","Roboto", "Anta", "Helvetica", "sans-serif","Kode Mono", "monospace"],
-        //     bodyFontSizeArray: ["12px","14px","16px","18px","24px","36px","48px","64px","72px"],
-        //     headerFontSizeArray: ["12px","14px","16px","18px","24px","36px","48px","64px","72px","96px"],
-        //     headerfontWeightList: ["300","400","500","600","700","800","900"],
-        //     fontSize: "16px",
-        //     lineHeightList: ["1","1.2","1.3","1.4","1.5","1.6","1.7","1.8","1.9","2.0"],
-        //     lineHeight: "1.5",
-        //     fontWeightList: ["100","200","300","400","500","600","700","800","900"],
-        //     buttonBorderRadiusList: ["0px","2px","4px","6px","8px","10px","12px","16px","18px"],
-        //     primaryColor: "violet",
-        //     primaryBackground: "#f5f5f5",
-        //     navbarBackgroundColor: "#212529",
-        //     navbarColor: "#fff",
-        //     body:{
-        //         fontSize:"16px",
-        //         fontWeight: "400",
-        //     },
-        //     headers:{
-        //         fontSize:"24px",
-        //         fontWeight: "400",
-        //     },
-        //     buttons: {
-        //         borderColor: "green",
-        //         borderRadius: "0px",
-        //         color: "blue",
-        //         backgroundColor: "pink",
-        //         padding:"1rem 3rem"
-        //     },
-        //     templateImages : {
-        //         heroImage: "/assets/images/shopping-image.jpeg",
-        //         promoImage: "assets/images/ecommerce-home-two-ladies.svg",
-        //         aboutHeroImage: "https://gitinspired.s3.amazonaws.com/aboutUsHero.jpg",
-        //         aboutFooterImage: "https://gitinspired.s3.amazonaws.com/aboutFooterImage.jpg",
-        //         contactImage: "https://gitinspired.s3.amazonaws.com/contactUsImage.png"
-        //     },
-        //     textAlign:"center",
-        //     headings: {
-        //         heroHeading: {
-        //             btnText: "SHOP NOW",
-        //             text: "level up your style with our summer collections",
-        //             color: "yellow",
-        //             fontSize: "72px",
-        //             lineHeight: "90px",
-        //             fontWeight: 700,
-        //             bgImage:{
-        //                 backgroundSize: "cover",
-        //                 width: "100%",
-        //                 height: "50rem",
-        //                 display: "flex",
-        //                 alignItems: "center",
-        //                 justifyContent: "center",
-        //                 flexDirection: "column",
-        //             }
-
-        //         },
-        //         headFontSize:"14px",
-        //         aboutHeading: {
-        //             text: "level up your style with our summer collections",
-        //             color: "#1C222B",
-        //             paragraph: "  ",
-        //             fontSize: "72px",
-        //             lineHeight: "90px",
-        //             fontWeight: 700
-        //         },
-        //         contactHeading: {
-        //             text: "CONTACT US",
-        //             color: "#1C222B",
-        //             fontSize: "72px",
-        //             lineHeight: "90px",
-        //             fontWeight: 700
-        //         },
+      }
       },
       sectionTwo: {
         twoSection: {
@@ -307,65 +219,68 @@ like: any =  [];
           },
         },
       },
-
+    
       aboutUs: {
-        ourStory:
-          'ShopNest was born from a vision to redefine the way you shop online. With years of experience in the eCommerce industry, we set out to create a one-stop destination where you can discover high-quality products, connect with trusted sellers, and enjoy seamless shopping from the comfort of your own nest',
-        whyShopWithUs: [
-          {
-            title: 'Convenience and Ease',
-            reason:
+        heading:{title: 'LEVEL UP YOUR STYLE WITH OUR SUMMER COLLECTIONS', img:'',
+              message:`Keep it simpleOne of the most essential website design principles
+              to follow is to keep everything simple. It must be easy to scroll through the page, and the content must be easy to read. Learn more`
+            },
+
+        ourStory: {title: 'OUR STORY', message:`ShopNest was born from a vision to redefine the way you shop online. With years of experience 
+              in the eCommerce industry, we set out to create a one-stop destination where you can discover high-quality products, connect with trusted sellers,
+              and enjoy seamless shopping from the comfort of your own nest`
+            },
+        whyShopWithUs: {
+          title1:'WHY SHOP WITH US',
+          
+            title2: 'Convenience and Ease',
+            reason2:
               "Shopping should be a breeze, and we've designed our platform with your convenience in mind. Browse, compare, and order products effortlessly, all with just a few clicks.",
-          },
-          {
-            title: 'Affordable, reliable shipping',
-            reason:
+          
+          
+            title3: 'Affordable, reliable shipping',
+            reason3:
               'Our shipping services offer reliability, convenience, and cost-effectiveness. We guarantee on-time delivery, secure tracking, and efficient customer service.',
-          },
-          {
-            title: 'Reliable shipping.',
-            reason:
+          
+          
+            title4: 'Reliable shipping.',
+            reason4:
               "Shopping should be a breeze, and we've designed our platform with your convenience in mind. Browse, compare, and order products effortlessly, all with just a few clicks.",
-          },
-        ],
-        team: [
+          
+        },
+        team: 
           {
-            id: 'team1',
-            name: 'Abigail Bell',
-            role: 'IBM',
-            picture: 'https://gitinspired.s3.amazonaws.com/team-3-user-1.png',
-          },
-          {
-            id: 'team2',
-            name: 'Alex King',
-            role: 'eBay',
-            picture: 'https://gitinspired.s3.amazonaws.com/team-3-user-2.jpg',
-          },
-          {
-            id: 'team3',
-            name: 'Adam King',
-            role: 'eBay',
-            picture: 'https://gitinspired.s3.amazonaws.com/team-3-user-3.jpg',
-          },
-          {
-            id: 'team4',
-            name: 'Mercy Richards',
-            role: 'Starbucks',
-            picture: 'https://gitinspired.s3.amazonaws.com/team-3-user-4.jpg',
-          },
-          {
-            id: 'team5',
-            name: 'Raymond Moses',
-            role: 'Starbucks',
-            picture: 'https://gitinspired.s3.amazonaws.com/team-3-user-5.png',
-          },
-          {
-            id: 'team6',
-            name: 'Jane Miles',
-            role: 'Facebook',
-            picture: 'https://gitinspired.s3.amazonaws.com/team-3-user-6.png',
-          },
-        ],
+            id1: 'team1',
+            name1: 'Abigail Bell',
+            role1: 'IBM',
+            picture1: '/assets/images/ecommerce-team-1.svg',
+       
+            id2: 'team2',
+            name2: 'Alex King',
+            role2: 'eBay',
+            picture2: '/assets/images/ecommerce-team-2.svg',
+          
+            id3: 'team3',
+            name3: 'Adam King',
+            role3: 'eBay',
+            picture3: '/assets/images/ecommerce-team-3.svg',
+         
+            id4: 'team4',
+            name4: 'Mercy Richards',
+            role4: 'Starbucks',
+            picture4: '/assets/images/ecommerce-team-4.svg',
+         
+            id5: 'team5',
+            name5: 'Raymond Moses',
+            role5: 'Starbucks',
+            picture5: '/assets/images/ecommerce-team-5.svg',
+         
+            id6: 'team6',
+            name6: 'Jane Miles',
+            role6: 'Facebook',
+            picture6: '/assets/images/ecommerce-team-6.svg',
+        
+        }
       },
       contactUs: {
         contactMessage: 'CONTACT US',
@@ -378,106 +293,39 @@ like: any =  [];
             '132 Dartmouth Street Boston, Massachusetts 02156 United States',
         },
       },
-      // pagesOrder:[{name:"home", value:"home", includePage: true}, {name:"about", value:"about us",IncludePage: false}, {name:"contact", value:"contact us", includePage: false}]
-    },
 
-    sectionTwo: {
-      twoSection: {
-        image: 'assets/images/ecommerce-home-two-ladies.svg',
-        text: {
-          discount: 'UPTO 40% OFF',
-          otherText: 'Special offers and great deals',
-        },
-      },
-    },
-    sectionThree: {
-      name: 'Featured Items',
-
-      twoSection: {
-        image: 'assets/images/ecommerce-home-two-ladies.svg',
-        text: {
-          discount: 'UPTO 40% OFF',
-          otherText: 'Special offers and great deals',
-        },
-      },
-    },
-
-    aboutUs: {
-      ourStory:
-        'ShopNest was born from a vision to redefine the way you shop online. With years of experience in the eCommerce industry, we set out to create a one-stop destination where you can discover high-quality products, connect with trusted sellers, and enjoy seamless shopping from the comfort of your own nest',
-      whyShopWithUs: [
-        {
-          title: 'Convenience and Ease',
-          reason:
-            "Shopping should be a breeze, and we've designed our platform with your convenience in mind. Browse, compare, and order products effortlessly, all with just a few clicks.",
-        },
-        {
-          title: 'Affordable, reliable shipping',
-          reason:
-            'Our shipping services offer reliability, convenience, and cost-effectiveness. We guarantee on-time delivery, secure tracking, and efficient customer service.',
-        },
-        {
-          title: 'Reliable shipping.',
-          reason:
-            "Shopping should be a breeze, and we've designed our platform with your convenience in mind. Browse, compare, and order products effortlessly, all with just a few clicks.",
-        },
-      ],
-      team: [
-        {
-          id: 'team1',
-          name: 'Abigail Bell',
-          role: 'IBM',
-          picture: 'https://gitinspired.s3.amazonaws.com/team-3-user-1.png',
-        },
-        {
-          id: 'team2',
-          name: 'Alex King',
-          role: 'eBay',
-          picture: 'https://gitinspired.s3.amazonaws.com/team-3-user-2.jpg',
-        },
-        {
-          id: 'team3',
-          name: 'Adam King',
-          role: 'eBay',
-          picture: 'https://gitinspired.s3.amazonaws.com/team-3-user-3.jpg',
-        },
-        {
-          id: 'team4',
-          name: 'Mercy Richards',
-          role: 'Starbucks',
-          picture: 'https://gitinspired.s3.amazonaws.com/team-3-user-4.jpg',
-        },
-        {
-          id: 'team5',
-          name: 'Raymond Moses',
-          role: 'Starbucks',
-          picture: 'https://gitinspired.s3.amazonaws.com/team-3-user-5.png',
-        },
-        {
-          id: 'team6',
-          name: 'Jane Miles',
-          role: 'Facebook',
-          picture: 'https://gitinspired.s3.amazonaws.com/team-3-user-6.png',
-        },
-      ],
-    },
-    contactUs: {
-      contactMessage: 'CONTACT US',
-      leftPanelInfo: {
-        heading: 'GET IN TOUCH',
-        pTag: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        phone: '+1012 3456 789',
-        email: 'demo@gmail.com',
-        location:
-          '132 Dartmouth Street Boston, Massachusetts 02156 United States',
-      },
-    },
     pagesOrder: [
       { name: 'home', value: 'home', includePage: true },
       { name: 'about', value: 'about us', IncludePage: false },
       { name: 'contact', value: 'contact us', includePage: false },
     ],
-  };
+  }
 
-  constructor() {}
+  constructor(public dialog: MatDialog) {}
+  updateText(text: any, ...args: string[]) {
+    text = text.replace(/<p>(.*?)<\/p>/g, '$1')
+    let targetObj = this.template;
+    for (let i = 0; i < args.length - 1; i++) {
+        const arg = args[i];
+        if (!targetObj[arg]) {
+            console.error(`Property '${arg}' not found in template.`);
+            return;
+        }
+        targetObj = targetObj[arg];
+    }
+    const lastArg = args[args.length - 1];
+    targetObj[lastArg] = text;
+}
+changeImageByLink(pathToProperty:string){
+  this.dialog.open(ImageLinkComponent, {
+    hasBackdrop: false
+  }).afterClosed().subscribe((imageLink) => {
+    const keys = pathToProperty.split('.');
+    const lastKey:any = keys.pop();
+    const targetObj = keys.reduce((acc, key) => {
+    return acc[key];
+  }, this.template);
+    targetObj[lastKey] = imageLink;
+  });
+}
 }
