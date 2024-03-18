87005b757246cebd3414e12b8b9a8657d4bf3281
Felix7854 <felix.hagan@amalitech.org>
2024-03-18 06:18:51 +0000

    feat: testing the review component

--- src/app/Pages/Authentication/CommonComponent/onboarding-step-one/onboarding-step-one.component.spec.ts
+++ src/app/Pages/Authentication/CommonComponent/onboarding-step-one/onboarding-step-one.component.spec.ts
@@ -1,21 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OnboardingStepOneComponent } from './onboarding-step-one.component';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { routes } from '../../../../app.routes';
 
 describe('OnboardingStepOneComponent', () => {
   let component: OnboardingStepOneComponent;
   let fixture: ComponentFixture<OnboardingStepOneComponent>;
+  let router: Router;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [OnboardingStepOneComponent]
+  beforeEach( () => {
+     TestBed.configureTestingModule({
+      imports: [OnboardingStepOneComponent, RouterTestingModule.withRoutes(routes)]
     })
     .compileComponents();
-    
+  });
+
+  beforeEach(()=> {
+    router = TestBed.inject(Router);
+    router.initialNavigation;
     fixture = TestBed.createComponent(OnboardingStepOneComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  })
 
   it('should create', () => {
     expect(component).toBeTruthy();
